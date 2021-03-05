facc258685e5b787a8dacf6a1501753c97fee8aa
giacomo9999 <gmarchesi888@gmail.com>
2021-03-05 17:30:28 -0500

    Basic bar graph working.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import BarGraph from "./BarGraph";
 function App() {
   const graphHeight = 300;
   const barGraphData = {
+    graphName: "My Bar Graph",
     data: [35, 10, 76, 32, 55, 29, 44, 66],
-    color: "yellowgreen",
-    gapPct: 10,
+    color: "#9CAAB9",
+    gap: 0.2,
   };
   return (
     <div className="container-outer">
-      <h1>Graph Name</h1>
+      <h1>{barGraphData.graphName}</h1>
       <BarGraph graphData={barGraphData} graphHeight={graphHeight} />
     </div>
   );
--- src/BarGraph.js
+++ src/BarGraph.js
@@ -2,29 +2,33 @@ const BarGraph = (props) => {
   console.log(props);
   // graphNumbers = array of values
   const graphNumbers = props.graphData.data;
-  let baseBarWidth = 100 / graphNumbers.length;
-  let baseGapWidth = 100 / graphNumbers.length - 1;
-  let barGap = (baseGapWidth * props.graphData.gapPct) / 100;
-  let adjBarWidth = baseBarWidth - barGap;
+  console.log(
+    graphNumbers.length,
+    props.graphData.gap,
+    graphNumbers.length + (graphNumbers.length - 1) * props.graphData.gap
+  );
+  let barWidth =
+    100 /
+    (graphNumbers.length + (graphNumbers.length - 1) * props.graphData.gap);
+  let barGap = props.graphData.gap * barWidth;
+
+  console.log(barWidth, barGap);
   let maxDataVal = Math.max(...graphNumbers);
   let heightUnit = props.graphHeight / maxDataVal;
 
   const graphBars = graphNumbers.map((entry, index) => (
     <rect
       key={"rect" + index}
-      x={(adjBarWidth + barGap) * index + "%"}
-      y={props.graphHeight - entry * heightUnit + 10}
+      x={(barWidth + barGap) * index + "%"}
+      y={props.graphHeight - entry * heightUnit}
       height={entry * heightUnit}
-      width={`${adjBarWidth}%`}
+      width={`${barWidth}%`}
       fill={props.graphData.color}
     />
   ));
 
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      height={`${props.graphHeight + 10}px`}
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" height={`${props.graphHeight}px`}>
       <rect x="0" y="0" width="100%" height={`${props.graphHeight + 10}px`} />
       {graphBars}
     </svg>
